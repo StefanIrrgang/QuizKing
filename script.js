67b776b840dce4fb4086769e6d5976d038e6694d
Stefan Irrgang <stvd29@gmail.com>
2023-07-12 09:27:18 +0200

    functions complete, added sounds

--- script.js
+++ script.js
@@ -34,6 +34,10 @@ let questions = [
 ];
 
 let currentQuestion = 0;
+let rightQuestion = 0;
+
+let AUDIO_SUCCESS = new Audio('audio/correct.mp3');
+let AUDIO_FAIL = new Audio('audio/wrong.mp3');
 
 function init() {
     document.getElementById('all-questions').innerHTML = questions.length;
@@ -44,8 +48,15 @@ function showQuestion() {
     if (currentQuestion >= questions.length) {
         document.getElementById('endScreen').style = '';
         document.getElementById('questionBody').style = 'display: none;';
+        document.getElementById('amountOfQuestions').innerHTML = questions.length;
+        document.getElementById('amountRightQuestions').innerHTML = rightQuestion;
+        document.getElementById('headerimage').src = 'img/trophy.png';
     } else {
         let question = questions[currentQuestion];
+        let percent = Math.round((currentQuestion + 1) / questions.length * 100);
+        document.getElementById('progressBar').innerHTML = `${percent}%`;
+        document.getElementById('progressBar').style = `width: ${percent}%`;
+
         document.getElementById('question-number').innerHTML = currentQuestion + 1;
         document.getElementById('questiontext').innerHTML = question['question'];
         document.getElementById('answer_1').innerHTML = question['answer_1'];
@@ -61,12 +72,15 @@ function answer(selection) {
     let idOfRightAnswer = `answer_${question['right_answer']}`;
 
     if (selectedQuestionNumber == question['right_answer']) {
-        console.log('Korrekt!')
+        console.log('Korrekt!');
+        AUDIO_SUCCESS.play();
+        rightQuestion++;
         document.getElementById(selection).parentNode.classList.add('bg-success');
     } else {
         console.log('Falsch!')
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        AUDIO_FAIL.play();
     }
     document.getElementById('next-button').disabled = false;
 }
@@ -87,4 +101,13 @@ function resetButtons() {
     document.getElementById('answer_3').parentNode.classList.remove('bg-success');
     document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
     document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
+
+function restartQuiz() {
+    document.getElementById('headerimage').src = '/img/pencil.jpg';
+    document.getElementById('endScreen').style = 'display: none;';
+    document.getElementById('questionBody').style = '';
+    currentQuestion = 0;
+    rightQuestion = 0;
+    init();
 }
